4e77c3c85ed14c0c43241cfc522e2dab80325f47
Matheus <ITERIS\matheus.nogueira>
2021-11-17 21:53:11 -0300

    add material to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsesModule } from './modules/uses/uses.module';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule, UsesModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/uses/uses.module.ts
+++ src/app/modules/uses/uses.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsesComponent } from './components/uses/uses.component';
 
-
-
 @NgModule({
-  declarations: [
-    UsesComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UsesComponent],
+  imports: [CommonModule],
+  exports: [UsesComponent],
 })
-export class UsesModule { }
+export class UsesModule {}
