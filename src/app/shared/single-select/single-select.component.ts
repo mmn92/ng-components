af0ea6565741fc40a2e424552fd03103a5acaa4e
Matheus <ITERIS\matheus.nogueira>
2021-11-17 23:01:21 -0300

    update itemList based on input

--- src/app/shared/single-select/single-select.component.ts
+++ src/app/shared/single-select/single-select.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 
+const updatedList = ['New Value', 'Updated', 'Mocked'];
 @Component({
   selector: 'app-single-select',
   templateUrl: './single-select.component.html',
@@ -12,7 +13,12 @@ export class SingleSelectComponent implements OnInit {
   itemList = ['First item', 'Second item', 'Third item'];
   // @Input()
   inputHandler = (val: Observable<any | string>) => {
-    val.subscribe(console.log);
+    val.subscribe((value) => {
+      console.log(value);
+      if (value.length > 4) {
+        this.itemList = updatedList;
+      }
+    });
   };
 
   inputControl = new FormControl('');
