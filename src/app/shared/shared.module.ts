ab727fac55e086c0d9ccd288b7128096c6dfaf0a
Matheus <ITERIS\matheus.nogueira>
2021-11-17 22:32:04 -0300

    create basic form structure

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SingleSelectComponent } from './single-select/single-select.component';
 
-
-
 @NgModule({
-  declarations: [
-    SingleSelectComponent
-  ],
+  declarations: [SingleSelectComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  exports: [
+    SingleSelectComponent,
+    CommonModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
--- src/app/shared/single-select/single-select.component.ts
+++ src/app/shared/single-select/single-select.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-single-select',
   templateUrl: './single-select.component.html',
-  styleUrls: ['./single-select.component.css']
+  styleUrls: ['./single-select.component.css'],
 })
 export class SingleSelectComponent implements OnInit {
+  // @Input()
+  itemList = ['First item', 'Second item', 'Third item'];
 
-  constructor() { }
+  inputControl = new FormControl('');
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.inputControl.valueChanges.subscribe(console.log);
   }
-
 }
