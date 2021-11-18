f2805cb5e0e25cfd8d37ac6b15e0d0914d9958fd
Matheus <ITERIS\matheus.nogueira>
2021-11-17 22:43:32 -0300

    add inputHandler function

--- src/app/shared/single-select/single-select.component.ts
+++ src/app/shared/single-select/single-select.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-single-select',
@@ -9,12 +10,16 @@ import { FormControl } from '@angular/forms';
 export class SingleSelectComponent implements OnInit {
   // @Input()
   itemList = ['First item', 'Second item', 'Third item'];
+  // @Input()
+  inputHandler = (val: Observable<any | string>) => {
+    val.subscribe(console.log);
+  };
 
   inputControl = new FormControl('');
 
   constructor() {}
 
   ngOnInit(): void {
-    this.inputControl.valueChanges.subscribe(console.log);
+    this.inputHandler(this.inputControl.valueChanges);
   }
 }
